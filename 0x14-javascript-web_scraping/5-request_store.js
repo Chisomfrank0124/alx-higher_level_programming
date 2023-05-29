afc0da3d7b4cbc1b2306195f27241531c9c2c38d
chisomfrank0124 <ikechukwuchisomfrank@gmail.com>
2023-05-29 04:12:08 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,12 +6,4 @@ request(process.argv[2], function (err, response, body) {
   if (err == null) {
     fs.writeFileSync(process.argv[3], body);
   }
-});#!/usr/bin/node
-const request = require('request');
-const fs = require('fs');
-
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    fs.writeFileSync(process.argv[3], body);
-  }
 });
